be920a36423f5247f1c75477eee1a7fd4653db1e
Given Lepita <Given.lepita@gmail.com>
2016-12-25 21:15:52 +0200

    Add files via upload

---
+++ Web Crawler/README.md
@@ -0,0 +1,29 @@
+# Overview
+
+This is an open source website crawler written in Python. Any contribution or ideas/suggestions are allowed.
+You are allowed to copy or modify this program in any way you want.
+
+
+# Prerequisites
+*** Install BeautifulSoup ***
+
+---------- Commands ----------
+
+Windows - 1. Open CLI(Command Line Interface/ Command Prompt)
+	  2. type "pip install bs4"
+
+Linux   - 1. Open Terminal/Shell
+	  2. type "sudo -s"
+	  3. Enter password
+	  4. type "pip install bs4"
+
+
+
+# Authors
+Given Lepita - initial work
+
+
+# Acknowledgments
+
+This is inspired by the beautiful work of open source projects. And I hope for contributions that will bring out the best of
+You as a programmer and the ability of the Python programming language.
---
+++ Web Crawler/Webcrawler.py
@@ -0,0 +1,78 @@
+# Project: Web crawler
+# Programmer: Given Lepita
+# Python Version: 3.x
+# Date: 2016
+import os
+from random import randint
+import urllib.request
+import urllib
+from urllib.error import URLError
+from bs4 import BeautifulSoup
+
+
+
+
+def crawler():
+    web_url = str(input('Enter URL to crawl: '))
+    correct_url = 'http://' + web_url  # concat with http protocol for valid url
+    request = urllib.request.urlopen(correct_url)
+    # http header
+    print("-------------------HTTP HEADERS-----------------")
+    print(request.info())
+    print('\n----------------- CONTENT-TYPE: ------------------\n', request.info().get('Content-Type'))
+    soup_object = BeautifulSoup(request, 'lxml')
+    
+    file = open('Response Code.html', 'w')
+    file.write(str(request))
+    file.close()
+    
+    print("\n------------------- IP Adress -------------------")
+    cmd = "host " + correct_url  # linux command to find url's ip address
+    process = os.popen(cmd)
+    ip_add = str(process.read())
+    count = 14
+    index_lvl = ip_add.find('has adress') + count
+    print('IP Address: ' + ip_add[index_lvl:])
+
+
+    print("\n--------------------- RETRIEVING LINKS ---------------")
+    try:
+        link_number= 1
+        for link in soup_object.find_all('a'):
+
+            print("Link {}'s Title: {}".format(link_number, link.text))
+            link_number += 1
+    except KeyError:
+        print("No \'href\' Attributes found")
+
+    print('\n------------------------ RETRIEVING PARAGRAPHS ----------------------------')
+
+    for para in soup_object.find_all('p'):
+        print(para.text)
+
+try:
+    crawler()
+except (URLError, ImportError, RuntimeError):
+    print("An error due to URLError, ImporError or RuntimeError Has Occurred.\nPlease Install The Necessary Modules")
+except:
+    print("An \'Unknown\' Error Occurred")        
+
+
+def take_img(url):
+    start_n = randint(1, 9000)
+    end_n = str(start_n) + "image.jpg" # if need be, change the image extension such as jpeg, jpg, png ect.
+    urllib.request.urlretrieve(url, end_n)
+
+
+print('\n------------------------ DOWNLOADING IMAGES ----------------------------')
+download = str(input("Enter the URL of the image to download: "))
+
+
+
+try:
+    take_img(download)
+except:
+    print("An \'Unknown\' Error Occurred.")
+
+
+
