f129812440b81e9e878832b5f45eebd37aba9f7a
Given Lepita <Given.lepita@gmail.com>
2023-07-21 01:04:22 +0200

    Update networkInfo.py
    Optimized the code.

--- Web Crawler/networkInfo.py
+++ Web Crawler/networkInfo.py
@@ -1,71 +1,60 @@
 import os
 import logging
 import requests
-import time
 from bs4 import BeautifulSoup
 from json import loads
 from prettytable import PrettyTable
-from urllib.request import urlopen, urlretrieve, Request
-from urllib.error import URLError
-from Webcrawler import WebCrawler
+from urllib.request import urlopen, urlretrieve
 
-
-# log file
 logging.basicConfig(filename="temp.log", level=logging.DEBUG)
 
 
-# Subclass of the crawler
-class NetworkIntelligence(WebCrawler):
-
+class WebCrawler:
     def __init__(self, base_url):
-        # inherit superclass variable
+        self.base_url = base_url
+
+
+class NetworkIntelligence(WebCrawler):
+    def __init__(self, base_url):
         super().__init__(base_url)
 
     def network_mapper(self):
-        # Do a fast NMAP scan
         logging.debug("Scan for open ports")
         command = "nmap -F " + self.base_url[8:]  # take out https://
         try:
             process = os.popen(command)
             output = str(process.read())
-            # -----------------
             style = '-' * 45
-            # -----------------
             print(style + " NMAP Fast Scan " + style)
             print(output)
         except Exception as e:
-            print("Error:   ", e)
+            print("Error: ", e)
 
-    def robotsFile(self):
-        # robots
+    def robots_file(self):
         try:
             # robots.txt
-            robotsReq = urlopen(self.base_url+"/robots.txt")
-            robots = robotsReq # store page
-            if robotsReq is not None:
-                bsRobots = BeautifulSoup(robots, "lxml")
-                style = ("-" * 45)
-                print(style + " Robots.txt " + style)
-                print(bsRobots.string)
-            else:
-                print("Robots.txt not found!")
+            robots_url = self.base_url + "/robots.txt"
+            robots_req = urlopen(robots_url)
+            robots = robots_req.read()
+            bs_robots = BeautifulSoup(robots, "lxml")
+            style = "-" * 45
+            print(style + " Robots.txt " + style)
+            print(bs_robots.get_text())
         except Exception as e:
-            print("Error:   ", e)
+            print("Error: ", e)
 
-    def whoIs(self):
-        # whois information [extras]
+    def who_is(self):
         command = "whois " + self.base_url[8:]
         process = os.popen(command)
         try:
             output = str(process.read())
             style = "-" * 45
-            print(style + " Whos is info " + style)
+            print(style + " Whois info " + style)
             print(output)
         except Exception as e:
-            print("Error:   ", e)
+            print("Error: ", e)
 
-    def getIP(self):
-        # getting an ip through the host service
+    def get_ip(self):
         command = "host {}".format(self.base_url[8:])
         process = os.popen(command)
         try:
@@ -74,47 +63,39 @@ class NetworkIntelligence(WebCrawler):
             print(style, " IP ADDRESS OF " + self.base_url[8:], style)
             print(output)
         except Exception as e:
-            print("Error ", e)
+            print("Error: ", e)
 
 
 class ScraperInfo(WebCrawler):
-
     def __init__(self, base_url):
         super().__init__(base_url)
 
-    def sourceCode(self, name='source code'):
+    def source_code(self, name='source code'):
         directory = str(os.getcwd())
         request = urlopen(self.base_url)
-        # create a bs4 object, change feature to HTML.parser
-        # if necessary
-        bsObj = BeautifulSoup(request, "lxml")
-        htmlCode = bsObj.prettify()
+        bs_obj = BeautifulSoup(request, "lxml")
+        html_code = bs_obj.prettify()
 
         file = "{}.html".format(name)
-        with open(directory+file, "w") as f:
-            f.write(htmlCode)
-        # close
-        f.close()
+        with open(os.path.join(directory, file), "w") as f:
+            f.write(html_code)
         print("Your file has been saved in {} as {}".format(directory, file))
 
-    def HTTP_headers(self):
+    def http_headers(self):
         request = urlopen(self.base_url)
         # HTTP headers
         TAB = "\t" * 5
         print(TAB, request.info())
 
-    def siteImages(self):
+    def site_images(self):
         links = set()
         request = urlopen(self.base_url)
-        bsObj = BeautifulSoup(request, "lxml")
-        for link in bsObj.find_all('a'):
+        bs_obj = BeautifulSoup(request, "lxml")
+        for link in bs_obj.find_all('a'):
             if 'src' in link.attrs:
-                # image has source
-                downloadURL = link.attrs['src']
-                print("[+] Storing => {}".format(downloadURL))
-                links.add(downloadURL)
-            else:
-                pass
+                download_url = link.attrs['src']
+                print("[+] Storing => {}".format(download_url))
+                links.add(download_url)
 
         index = 0
         name = "{} .jpg".format(str(index))
@@ -128,40 +109,31 @@ class ScraperInfo(WebCrawler):
 
 
 class IPLookUp(WebCrawler):
+    def __init__(self, ip_address):
+        self.ip_address = ip_address
 
-    def __init__(self, ipAddress):
-        self.ipAddress = ipAddress
+    def get_information(self):
+        try:
+            url = "https://freegeoip.app/json/" + str(self.ip_address)
+            response = requests.get(url)
+            json_data = response.json()
 
-    def getInformation(self):
-        # JSON FILE using Freegeoip API
+            table = PrettyTable()
+            table.field_names = [
+                "Region", "Country Code", "Latitude",
+                "Longitude", "IP Address", "Zip Code",
+                "Time Zone", "Country", "City"
+            ]
+            table.add_row([
+                json_data['region_name'], json_data['country_code'],
+                json_data['latitude'], json_data['longitude'],
+                json_data['ip'], json_data['zip_code'],
+                json_data['time_zone'], json_data['country_name'],
+                json_data['city']
+            ])
+            # align to the right
+            table.align = "r"
+            print(table)
+        except Exception as e:
+            print("Error: ", e)
 
-        request = urlopen("https://freegeoip.net/json/"+str(self.ipAddress))
-        request = request.read().decode("utf-8")
-        jsonFile = loads(request)
-
-        region = jsonFile['region_name']
-        countryCode = jsonFile['country_code']
-        latitude = jsonFile['latitude']
-        longitude = jsonFile['longitude']
-        ipAddr = jsonFile['ip']
-        zipCode = jsonFile['zip_code']
-        timeZone = jsonFile['time_zone']
-        countryName = jsonFile['country_name']
-        city = jsonFile['city']
-
-        table = PrettyTable()
-        table.field_names = [
-            "Region", "Country Code", "Latitude",
-            "Longitude", "IP Address", "Zip Code",
-            "Time Zone", "Country", "City"
-        ]
-        table.add_row(
-            [
-                region, countryCode, latitude,
-                longitude, ipAddr, zipCode,
-                timeZone, countryName, city
-                ]
-        )
-        # align to the right
-        table.align = "r"
-        print(table)
