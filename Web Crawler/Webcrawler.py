fc6de1132c942e1625dbd04c79a3d8d2f18417c1
Given Lepita <Given.lepita@gmail.com>
2023-07-21 01:06:36 +0200

    Update Webcrawler.py
    Optimized code.

--- Web Crawler/Webcrawler.py
+++ Web Crawler/Webcrawler.py
@@ -3,9 +3,6 @@ class WebCrawler():
         self.base_url = base_url
 
     def get_absolute_url(self):
-        if self.base_url[:7] == "https://":
+        if self.base_url.startswith(("http://", "https://")):
             return self.base_url
-        elif self.base_url[:6] == "http://":
-            return self.base_url
-        else:
-            return "https://" + self.base_url
+        return "https://" + self.base_url
